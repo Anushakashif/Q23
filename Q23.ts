dcc46da79055109e4156d6b297269f0022fa0f7e
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-21 16:18:53 -0700

    Add files via upload

---
+++ Q23.ts
@@ -0,0 +1,34 @@
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your 
+// prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to false.
+
+let car = 'maybach';
+// console.log("Is car == 'maybach' I predict True otherwise I predict False")
+console.log(car == 'maybach')
+console.log(car == 'phantom')
+
+let num: any = 23
+// console.log("Is num == (in number) ? I predict true otherwise I predict False")
+console.log(num === 23)
+console.log(num === "23")
+
+let flower: string = "rose"
+console.log(flower === 'rose')
+console.log(flower === 'lily')
+
+let country: string = "US"
+console.log(country == 'US')
+console.log(country == 'UK')
+
+let book: string = "Twisted Love"
+console.log(book === 'Twisted Games')
+console.log(book === 'Twisted Love')
